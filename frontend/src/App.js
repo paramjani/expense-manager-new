445d8df441d73b4c301c90a79203230522ea950b
paramjani <125356533+paramjani@users.noreply.github.com>
2025-03-13 05:44:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5001";
+const API_URL = "https://expense-manager-new-backend.onrender.com/";
 
 function App() {
     const [expenses, setExpenses] = useState([]);
